99cc3371cdf2ec4ae44c49b0b0a9d36bd1051bbf
James Cape <james.cape@gmail.com>
2019-03-16 19:16:29 -0600

    Shorten guess, category, answer reformatting by adding a variable

--- lib/card.rb
+++ lib/card.rb
@@ -10,10 +10,7 @@ class Card
 # Normalizes answers for errant spaces and capitalizations.
     @answer   = answer.to_s.strip.downcase.capitalize.delete(' ')
     @category = category.to_s.strip.downcase.delete(' ').capitalize.to_sym
+###
   end
 
 end
-
-# Adding comment to change file and create pull.
-
-# Add a second comment
